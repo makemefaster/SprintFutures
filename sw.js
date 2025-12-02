c21ed5391fdaf6f236cfd5f9b9769c4ba4915ff8
makemefaster <42447944+makemefaster@users.noreply.github.com>
2025-12-02 08:13:33 +0000

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,6 @@
-const CACHE_NAME = 'race-suite-v1';
+// INCREASE THIS VERSION NUMBER EVERY TIME YOU UPDATE CODE
+const CACHE_NAME = 'race-suite-v3'; 
+
 const urlsToCache = [
   './',
   './index.html',
@@ -8,9 +10,25 @@ const urlsToCache = [
 ];
 
 self.addEventListener('install', event => {
+  self.skipWaiting(); // Forces new Service Worker to take over immediately
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('activate', event => {
+  // Delete old caches (v1, v2, etc)
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
   );
 });
 
